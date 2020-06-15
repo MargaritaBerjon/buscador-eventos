6aea9838c493b379fb1450f657c2ef959f4a4142
Margui <margui.berjon@gmail.com>
2020-06-15 12:58:30 +0200

    feature: call API form option values

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -1,10 +1,17 @@
 class EventBrite {
-  constructor(){
-    this.token_OAuth = 'NVQBVPBPGY5ZN5FPJPGKWHS667GMAMD5FDUALJNUFPQUWHAN5E';
-    this.order ='date';
+  constructor() {
+    this.token_OAuth = '2CZWF5RL3SHAE7RFJVG3';
+    this.order = 'date';
   }
 
-  async obtainCategories(){
-    
+  async obtainCategories() {
+    const responseCategories = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_OAuth}`);
+
+    const categories = await responseCategories.json();
+
+    return {
+      categories
+    }
   }
+
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -6,10 +6,23 @@ class Interface {
   }
 
   init() {
-
+    this.printCategories();
   }
 
-  printCategories(){
-    const categoriesList = eventbrite.obtainCategories();
+  printCategories() {
+    const categoriesList = eventbrite.obtainCategories()
+      .then(categories => {
+        const categoriesClean = categories.categories.categories;
+
+        const selectCategories = document.getElementById('listado-categorias');
+        categoriesClean.map(category => {
+          const option = document.createElement('option')
+          option.value = category.id;
+
+          option.appendChild(document.createTextNode(category.name_localized));
+          selectCategories.appendChild(option)
+        });
+
+      })
   }
 }
